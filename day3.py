bc9a2c5fea4f539f1b757ee4095d09ac1c70aaff
ritteek08 <ritteekkumar.2403120@srec.ac.in>
2025-12-25 19:43:36 +0530

    file added

---
+++ day3.py
@@ -0,0 +1,23 @@
+#Code6
+#Area of Circle
+import math
+print("To calculate the Area of Circle:")
+r= float(input("Enter the radius of the circle: "))
+a= math.pi * r** 2
+print(f"The area of the circle is: {a:.3f} sq.units\n")
+
+#Code7
+#Area of Rectangle  
+print("To calculate the Area of Rectangle:")
+l= float(input("Enter the length of the rectangle: "))
+b= float(input("Enter the breadth of the rectangle: "))
+a2= l * b
+print(f"The area of the rectangle is: {a2:.3f} sq.units\n")
+
+#Code8
+#Area of Triangle
+print("To calculate the Area of Triangle:")
+b= float(input("Enter the base of the triangle: "))
+h= float(input("Enter the height of the triangle: "))
+a3= 0.5 * b * h
+print(f"The area of the triangle is: {a3:.3f} sq.units\n")
