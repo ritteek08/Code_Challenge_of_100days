da62caeb6b77c933b930915bfdcab163c17a2904
ritteek08 <ritteekkumar.2403120@srec.ac.in>
2025-12-27 22:16:59 +0530

    file added

---
+++ day5.py
@@ -0,0 +1,12 @@
+#Code12
+#Find largest of two numbers
+print("Find largest of two numbers")
+a=int(input("Enter the first variable (a):"))
+b=int(input("Enter the second variable (b):"))
+
+if a>b:
+    print("a is larger than b")
+elif b>a:
+    print("b is larger than a")
+else:
+    print("a is equal to b")
