2976e64f8359eaf8fc41cd2e79c42ed5b609f392
ritteek08 <ritteekkumar.2403120@srec.ac.in>
2025-12-30 21:29:38 +0530

    file added

---
+++ day8.py
@@ -0,0 +1,7 @@
+#Check leap year
+print("Enter a year:")
+year = int(input())
+if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
