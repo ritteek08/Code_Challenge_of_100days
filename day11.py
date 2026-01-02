d51d053b139091fd32623ea1ad8d38b5a1f4ec46
ritteek08 <ritteekkumar.2403120@srec.ac.in>
2026-01-02 20:12:49 +0530

    file added

---
+++ day11.py
@@ -0,0 +1,7 @@
+#Factorial of a number:
+print("Factorial Calculator")
+n = int(input("Enter a number: "))
+factorial = 1
+for i in range(1, n + 1):
+    factorial *= i
+print(f"The factorial of {n} is {factorial}")
