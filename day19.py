e5559a2eb261c987ccd06f99e743917b3bb427be
ritteek08 <ritteekkumar.2403120@srec.ac.in>
2026-01-10 18:32:26 +0530

    file added

---
+++ day19.py
@@ -0,0 +1,15 @@
+#Prime number check
+print("Check if a number is prime")
+num = int(input("Enter a number: "))
+is_prime = True
+if num < 2:
+    is_prime = False
+else:
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+if is_prime:
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is not a prime number.")
