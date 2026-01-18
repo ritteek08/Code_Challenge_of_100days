d97a2c97b4d38e4167c5906f0d0df5728f06941d
ritteek08 <ritteekkumar.2403120@srec.ac.in>
2026-01-18 18:53:41 +0530

    file added

---
+++ day27.py
@@ -0,0 +1,9 @@
+#Sum of list elements
+def sum_list(numbers):
+    total = 0
+    for num in numbers:
+        total += num
+    return total
+
+numbers = [1, 2, 3, 4, 5]
+print("Sum of list elements:", sum_list(numbers))
