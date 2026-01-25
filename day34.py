65a95d80be0dbc76dd2ca2a3d1241ac43c315e83
ritteek08 <ritteekkumar.2403120@srec.ac.in>
2026-01-25 18:58:28 +0530

    file added

---
+++ day34.py
@@ -0,0 +1,13 @@
+#Selection sort
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+numbers = [64, 34, 25, 12, 22, 11, 90]
+sorted_numbers = selection_sort(numbers)
+print("Sorted array is:", sorted_numbers)
