3918e7e7f5e04e71a67dbe78bd719195c0b6227b
ritteek08 <ritteekkumar.2403120@srec.ac.in>
2026-02-10 22:46:07 +0530

    file added

---
+++ day49.py
@@ -0,0 +1,10 @@
+my_dict = {
+    "name": "Alice",
+    "age": 30,
+    "city": "New York"
+}
+print(my_dict["name"])
+print(my_dict.get("age"))
+my_dict["email"] = "alice@example.com" 
+my_dict["age"] = 31
+print(my_dict)
