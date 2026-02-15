0ae4d83beb056b646e2950c25e6d37aa57fb72fb
ritteek08 <ritteekkumar.2403120@srec.ac.in>
2026-02-15 21:14:11 +0530

    file added

---
+++ day54.py
@@ -0,0 +1,11 @@
+#Convert tuple to list
+def tuple_to_list(tup):
+    print(tup)
+    return list(tup)
+my_tuple = (1, 2, 3, 4, 5)
+my_list = tuple_to_list(my_tuple)
+print(my_list)
+n=int(input("number of inputs: "))
+for i in range(n):
+    element = input("Enter element: ")
+    my_list.append(element)
