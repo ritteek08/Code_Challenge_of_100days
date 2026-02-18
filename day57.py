b6dbb26cec34c0edffe5a6dad2a325f38042ffa5
ritteek08 <ritteekkumar.2403120@srec.ac.in>
2026-02-18 19:05:28 +0530

    file added

---
+++ day57.py
@@ -0,0 +1,14 @@
+#Set difference in python
+def difference(set1, set2):
+    return set1 - set2
+set1 = {1, 2, 3}
+set2 = {3, 4, 5}
+result = difference(set1, set2)
+print(result)
+n=int(input("number of inputs: "))
+for i in range(n):
+    element = int(input("Enter element: "))
+    set1.add(element)
+print(set1)
+result = difference(set1, set2)
+print(result)
