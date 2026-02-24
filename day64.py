e99957d51be4f4ec0749aeb3e6e9ae35ba9d774f
ritteek08 <ritteekkumar.2403120@srec.ac.in>
2026-02-24 19:28:05 +0530

    file added

---
+++ day64.py
@@ -0,0 +1,10 @@
+#Count lines in file in python
+f=  open("test.txt", "r")
+lines = f.readlines()
+line_count = len(lines)
+print(f"Number of lines in the file: {line_count}\n")
+
+with open("test.txt", "r") as f:
+    lines = f.readlines()
+    line_count = len(lines)
+    print(f"Number of lines in the file: {line_count}")
