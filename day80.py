5b2a848f59e0a67856849abafdd89745e05a39ef
ritteek08 <ritteekkumar.2403120@srec.ac.in>
2026-03-23 18:10:45 +0530

    file added

---
+++ day80.py
@@ -0,0 +1,16 @@
+#Recursive Fibonacci series of n numbers
+print("Recursive Fibonacci series of n numbers")
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib_series = fibonacci(n - 1)
+        fib_series.append(fib_series[-1] + fib_series[-2])
+        return fib_series
+
+n = int(input("Enter the number of Fibonacci numbers to generate: "))
+print(f"The first {n} numbers in the Fibonacci series are: {fibonacci(n)}")
