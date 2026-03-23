eb116022dd8488ecfb147bd32cd6a7362a08f84d
ritteek08 <ritteekkumar.2403120@srec.ac.in>
2026-03-23 18:58:16 +0530

    file added

---
+++ day82.py
@@ -0,0 +1,8 @@
+#Random number generator in Python
+import random
+
+random_number = random.randint(1, 18)
+print(f"Random number: {random_number}")
+
+random_float = random.random()
+print(f"Random float: {random_float}")
